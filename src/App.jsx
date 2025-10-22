3eda2e089cc875549b6b7f1bf8bfe43aeebb929d
Shindu Astawa <shinduastawa@gmail.com>
2025-10-22 13:32:07 +0800

    Add Route Keranjang Belanja

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Frontend/Pages/HomePage';
 import Login from './Frontend/Pages/LoginPage';
 import Registrasi from './Frontend/Pages/RegistrasiPage';
-
+import Keranjang from './Frontend/Pages/ChartPage'
 function App() {
   return (
     <div>
@@ -12,6 +12,8 @@ function App() {
         <Route path="/Home" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Registrasi" element={<Registrasi />} />
+        <Route path="/Keranjang" element={<Keranjang />} />
+
       </Routes>
     </div>
   );
